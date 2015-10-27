ff9e2e2d4cfd3eb70fc835ef8e33fdc7a8627370
Alexander Daniel <AlexanderRDaniel@gmail.com>
2015-10-27 12:32:34 -0500

    perform immediately when not in transaction
    A given block will now be performed immediately when not in a transaction.

--- lib/when_committed.rb
+++ lib/when_committed.rb
@@ -1,30 +1,42 @@
 require 'when_committed/version'
+require 'active_record'
 
-module WhenCommitted
-  module ActiveRecord
-    def self.included(base)
-      base.after_commit :run_when_committed_callbacks
-      base.after_rollback :clear_when_committed_callbacks
+module WhenCommitted::ActiveRecord
+
+  extend ActiveSupport::Concern
+
+  included do
+    class_eval do
+      after_commit :run_when_committed_callbacks
+      after_rollback :clear_when_committed_callbacks
     end
+  end
 
-    def when_committed(&block)
+  def when_committed(&block)
+    if in_transaction?
       when_committed_callbacks << block
+    else
+      block.call
     end
+  end
 
-    private
+  private
 
-    def when_committed_callbacks
-      @when_committed_callbacks ||= []
-    end
+  def when_committed_callbacks
+    @when_committed_callbacks ||= []
+  end
 
-    def run_when_committed_callbacks
-      when_committed_callbacks.each {|cb| cb.call}
-      clear_when_committed_callbacks
-    end
+  def run_when_committed_callbacks
+    when_committed_callbacks.each {|cb| cb.call}
+    clear_when_committed_callbacks
+  end
 
-    def clear_when_committed_callbacks
-      when_committed_callbacks.clear
-    end
+  def clear_when_committed_callbacks
+    when_committed_callbacks.clear
+  end
+
+  def in_transaction?
+    ActiveRecord::Base.connection.open_transactions != 0
   end
 end
 
